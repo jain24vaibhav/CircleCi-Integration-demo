5c61097bb7d434f549bdbb0159169e5f79723070
Victor <csbsvits@gmail.com>
2021-02-28 09:59:01 +0530

    first commit

---
+++ src/test/java/com/vaibhavjain/circleci/CircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavjain.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

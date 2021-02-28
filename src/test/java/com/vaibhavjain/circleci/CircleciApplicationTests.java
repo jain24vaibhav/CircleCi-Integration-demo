d6772fd778b93798385992fdad3126fec544c0ac
Victor <csbsvits@gmail.com>
2021-02-28 10:13:46 +0530

    VJ-1 : added rest controller

--- src/test/java/com/vaibhavjain/circleci/CircleciApplicationTests.java
+++ src/test/java/com/vaibhavjain/circleci/CircleciApplicationTests.java
@@ -1,13 +1,25 @@
 package com.vaibhavjain.circleci;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vaibhavjain.circleci.controller.HelloController;
 
 @SpringBootTest
 class CircleciApplicationTests {
+	
+	@Autowired
+	private HelloController helloController;
 
 	@Test
 	void contextLoads() {
+		
+		String response = helloController.hello();
+		assertEquals(response, "Hello CircliCi");
+		
 	}
 
 }
